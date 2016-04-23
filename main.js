c88178b95f96b03ddcb64ad8f204a10d5974a7e5
imrokas <rokas@imrokas.com>
2016-04-22 20:26:56 -0700

    question 2

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ function searchBooks(library, key, keyword) {
 	// return all the matches
 	return matches;
 }
+
+//Write a function extends that accepts two objects as arguments, and extends all of the key/value pairs of the second one to the first one.
+function extends(obj1, obj2) {
+	// go over all the keys in obj2
+	for(var key in obj2) {
+		// check if key exist in obj
+		if(!obj1.hasOwnProperty(key)) { // if (!obj1[key])
+			// if it doesn't just add
+			obj1[key] = obj2[key];
+		}
+
+		/* v2 extend obj1 and overwright it's values
+			//[no need for if statement anymore]
+			obj1[key] = obj2[key];
+		*/
+	}
+
+	//return obj1
+	return obj1;
+}
